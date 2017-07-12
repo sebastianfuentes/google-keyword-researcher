b956103968543b8f1f0f15ddec58050a4a778047
Sebastian Fuentes <sebas6297@gmail.com>
2017-07-11 19:23:48 -0500

    :fire: fix in directories

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 
 const keywords = require('./keywords.js');
 
-let cleanedKeywords = keywords.init(process.argv[2] ? (process.argv[2].indexOf(".csv") !== -1 ? process.argv[2] : `${process.argv[2]}.csv`) : undefined, true);
+let cleanedKeywords = keywords.init(process.argv[2] ? (process.argv[2].indexOf(".csv") !== -1 ? `./csv/${process.argv[2]}` : `./csv/${process.argv[2]}.csv`) : undefined, true);
